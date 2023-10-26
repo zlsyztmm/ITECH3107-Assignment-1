2cd7c2aad42318bafad0b96997e7f4c24ef2d0f8
zlsyztmm <2955993684@qq.com>
2023-10-26 20:36:48 +0800

    01
    The initial version basically implements the required functions.

---
+++ Jiayi zhang 30395696_01/app/src/main/java/au/edu/federation/itech3107/studentattendance30395696/activity/ClassItemActivity.java
@@ -0,0 +1,196 @@
+package au.edu.federation.itech3107.studentattendance30395696.activity;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnTimeSelectChangeListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.TimePickerView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import au.edu.federation.itech3107.studentattendance30395696.R;
+import au.edu.federation.itech3107.studentattendance30395696.db.ClassDatabase;
+import au.edu.federation.itech3107.studentattendance30395696.entity.Course;
+import au.edu.federation.itech3107.studentattendance30395696.jiekou.CallBackUtils;
+
+public class ClassItemActivity extends Activity {
+    private EditText className;
+    private EditText classNum;
+    private TextView startTime;
+    private TextView endTime;
+    private Button subBtn;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_item);
+        initView();
+
+
+        pick();
+    }
+
+    private void pick() {
+//        Calendar startDate = Calendar.getInstance();
+//        Calendar endDate = Calendar.getInstance();
+//
+//        startDate.set(2023,0,2);
+//        endDate.set(2025,1,5);
+//
+//        TimePickerView pvTime = new TimePickerBuilder(this, new OnTimeSelectListener() {
+//            @Override
+//            public void onTimeSelect(Date date, View v) {
+//                Log.i("TAG123456", "onTimeSelect: " + date);
+//            }
+//        })
+//        .setTimeSelectChangeListener(new OnTimeSelectChangeListener() {
+//            @Override
+//            public void onTimeSelectChanged(Date date) {
+//
+//            }
+//        })
+//        .setType(new boolean[]{true, true, true, true, true, true})
+//        .setItemVisibleCount(6)
+//        .setLineSpacingMultiplier(2.0f)
+//        .setRangDate(startDate,endDate)
+//        .isAlphaGradient(true)
+//        .build();
+//        pvTime.show();
+
+    }
+
+    private void initView() {
+        classNum = (EditText) findViewById(R.id.class_num);
+        className = (EditText) findViewById(R.id.class_name);
+        startTime = (TextView) findViewById(R.id.start_time);
+        endTime = (TextView) findViewById(R.id.end_time);
+        subBtn = (Button) findViewById(R.id.sub_btn);
+
+        setTime();
+
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(classNum.getText().toString())) {
+                    Toast.makeText(ClassItemActivity.this, "课程号还未填写", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(className.getText().toString())) {
+                    Toast.makeText(ClassItemActivity.this, "课程名字还未填写", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (startTime.getText().toString().trim().equals("点击选择开始时间")) {
+                    Toast.makeText(ClassItemActivity.this, "你还未选择开始时间", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (endTime.getText().toString().trim().equals("点击选择结束时间")) {
+                    Toast.makeText(ClassItemActivity.this, "你还未选择结束时间", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                final Course course = new Course(System.currentTimeMillis(),
+                        classNum.getText().toString(),
+                        className.getText().toString(),
+                        startTime.getText().toString()
+                        , endTime.getText().toString());
+
+
+                CallBackUtils.doCallBackMethod(course);
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        ClassDatabase.getAppDatabase().classDao().insertClass(course);
+                        finish();
+                    }
+                }.start();
+            }
+        });
+    }
+
+    boolean checkFlag = false;
+    Date date1;
+
+    private void setTime() {
+
+        TimePickerView pvTime = new TimePickerBuilder(ClassItemActivity.this, new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date, View v) {
+                checkFlag = true;
+                startTime.setText((date.getYear() + 1900)
+                        + " " + (date.getMonth() + 1)
+                        + " " + date.getDate());
+
+                date1 = dealDays(date, 84);
+                endTime.setText((date1.getYear() + 1900) + " " + (date1.getMonth() + 1) + " " + (date1.getDate()));
+
+            }
+        }).build();
+
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pvTime.show();
+            }
+        });
+
+
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkFlag) {
+
+                    final String[] s = startTime.getText().toString().split(" ");
+
+                    Calendar startDate = Calendar.getInstance();
+                    Calendar endDate = Calendar.getInstance();
+
+                    startDate.set(Integer.valueOf(s[0]), Integer.valueOf(s[1]), Integer.valueOf(s[2]));
+
+                    final int yea = date1.getYear() + 1900;
+                    final int mon = date1.getMonth() + 1;
+                    final int dat = date1.getDate();
+                    endDate.set(yea, mon, dat);
+                    TimePickerView pvTime2 = new TimePickerBuilder(ClassItemActivity.this, new OnTimeSelectListener() {
+                        @Override
+                        public void onTimeSelect(Date date, View v) {
+                            endTime.setText((date.getYear() + 1900)
+                                    + " " + (date.getMonth() + 1)
+                                    + " " + date.getDate());
+                        }
+                    }).setRangDate(startDate, endDate).build();
+                    pvTime2.show();
+                } else {
+                    Toast.makeText(ClassItemActivity.this, "请选择开始时间", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    public static Date dealDays(Date date, int amount) {
+        Calendar now = Calendar.getInstance();
+        now.setTime(date);
+        now.set(Calendar.DATE, now.get(Calendar.DATE) + amount);
+        return now.getTime();
+    }
+}
